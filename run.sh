a340aa2aafca7618ef4c3a75fe3ababafb9ce871
anlai <alan.n.lai@gmail.com>
2024-08-17 19:47:49 -0700

    fixed syntax error

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 GROUP=${1:-all}
 
-if [ $GROUP -ne 'all' ]; then
-  ansible-playbook -i ./config/inventory.yml -l "$GROUP" ./main.yml -K  
+if [[ "$GROUP" != 'all' ]]; then
+  ansible-playbook -i ./config/inventory.yml -l "$GROUP" ./main.yml -K 
 else
   ansible-playbook -i ./config/inventory.yml ./main.yml -K
 fi
